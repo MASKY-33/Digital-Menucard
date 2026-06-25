1c1b66a08304cc6e67e59937a0871a00b6d01abc
MASKY-33 <masky.somebody33@gmail.com>
2026-06-25 17:09:25 +0200

    Create digital_menucard.py

---
+++ digital_menucard.py
@@ -0,0 +1,34 @@
+### Learning Dictionaries (dicts) by building a first little system about it
+
+## Digital Menucard
+
+menucard = {
+    "water" : 2.50,
+    "thee" : 2.20,
+    "cola" : 2.80
+}
+
+drink = menucard["water"]
+print(f"Price of water: {drink}")
+
+
+menucard["orange"] = 2.70
+
+menucard["water"] = 3.00
+
+print(menucard)
+
+
+changed_price = menucard["water"]
+# print(f"New water price: {changed_price}")
+
+
+## Below here, The System Control, if there's a certain drink on the menu-card or not
+
+search_word = "coffee"
+
+if search_word in menucard:
+    new_drink = menucard[search_word]
+    print(f"New drink price: {new_drink}")
+else:
+    print(f"{search_word} is not on the menucard.")
